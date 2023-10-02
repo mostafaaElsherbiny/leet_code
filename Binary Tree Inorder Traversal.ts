18e38d1b9be378bc5955ae9b6c10cd177dca34c5
MUSTAFA <elsherbiny.m@itcomp.org>
2023-10-02 04:10:28 +0300

    Binary Tree Inorder Traversal.ts

---
+++ Binary Tree Inorder Traversal.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  function inorderList(node: TreeNode | null, list: number[] = []) {
+    if (!node) return [];
+    if (node.left) {
+      inorderList(node.left, list);
+    }
+    list.push(node.val);
+    if (node.right) {
+      inorderList(node.right, list);
+    }
+    return list;
+  }
+
+  return inorderList(root, []);
+}
